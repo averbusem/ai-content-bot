7aad7592b226ec8ef112a4d3ca2d7ca5bdb50f51
averbusem <semyontechguru@gmail.com>
2025-11-15 08:43:10 +0300

    feat(redis): add Redis integration

--- src/config.py
+++ src/config.py
@@ -6,5 +6,18 @@ class Settings(BaseSettings):
 
     BOT_TOKEN: str = ""
 
+    REDIS_HOST: str = ""
+    REDIS_PORT: str = ""
+    REDIS_PASSWORD: str = ""
+    REDIS_DB: str = "0"
+
+    @property
+    def REDIS_URL(self):
+        url = (
+            f"redis://:{self.REDIS_PASSWORD}@"
+            f"{self.REDIS_HOST}:{self.REDIS_PORT}"
+            f"/{self.REDIS_DB}"
+        )
+        return url
 
 settings = Settings()
