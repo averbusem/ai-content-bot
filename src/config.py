856f93224b4e4e68e4443c96507ad31ea54acb03
averbusem <semyontechguru@gmail.com>
2025-11-24 22:59:37 +0300

    chore(models): add SQLAlchemy models

--- src/config.py
+++ src/config.py
@@ -13,6 +13,20 @@ class Settings(BaseSettings):
     # Telegram Bot
     BOT_TOKEN: str = ""
 
+    DB_HOST: str = ""
+    DB_PORT: str = ""
+    DB_USER: str = ""
+    DB_PASS: str = ""
+    DB_NAME: str = ""
+
+    @property
+    def DATABASE_URL(self):
+        url = (
+            f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        )
+        return url
+
     REDIS_HOST: str = ""
     REDIS_PORT: str = ""
     REDIS_PASSWORD: str = ""
